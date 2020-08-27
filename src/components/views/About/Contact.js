b74cda9199ca79ef1db89f1af912af1334160d8d
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-08-27 04:31:05 +0200

    Fixed css for tablets

--- src/components/views/About/Contact.js
+++ src/components/views/About/Contact.js
@@ -12,7 +12,7 @@ function Contact() {
       <ul>
         <h3><span><span>Social</span></span></h3>
         <li><span><span><a href='https://twitter.com/BartekWiejak' target='blank'><Button type='black'>Twitter</Button></a></span></span></li>
-        <li><span><span><a href='https://github.com/bartlomiejwiejak' target='blank'><Button type='black'>Github</Button></a></span></span></li>
+        <li><span><span><a href='https://github.com/bartlomiejwiejak/bartlomiej-wiejak-portfolio' target='blank'><Button type='black'>Github</Button></a></span></span></li>
         <li><span><span><a href='https://www.linkedin.com/in/bart%C5%82omiej-wiejak-4b9a891b3/' target='blank'><Button type='black'>Linkedin</Button></a></span></span></li>
       </ul>
     </div>
