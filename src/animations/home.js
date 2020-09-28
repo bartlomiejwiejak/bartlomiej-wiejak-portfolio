e55cc335a5c169177dad1f8eb9ff85c92a689be4
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-09-28 21:08:08 +0200

    Improved perfomrance on mobile home page

--- src/animations/home.js
+++ src/animations/home.js
@@ -1,22 +1,22 @@
 import gsap from 'gsap';
 
-import isMobile from '../functions/isMobile';
 import { toDark } from '../functions/handleBackground';
 
 export const homeEnter = (callafter) => {
   const timeout = toDark(500);
   setTimeout(() => {
     callafter();
-    if (!isMobile()) {
+    if (window.innerWidth >= 640) {
       gsap.to('.light', 1, { scale: .5, delay: .5, ease: 'power2.out' })
+      gsap.to('.home .home__welcome--shadow span span', 1.2, { y: 0, stagger: .15, ease: 'power2.out', opacity: 1 });
     } else {
       gsap.to('.light', 1, { scale: .5, ease: 'power2.out', delay: 1.5 })
+      gsap.to('.home .home__welcome--shadow span span', 1.2, { opacity: 1, delay: 1.5 });
     }
     gsap.to('.contact__item .button', 1.5, { y: 0, ease: 'power2.out' });
     gsap.to('.player__content i', 1.5, { y: 0, autoAlpha: 1, ease: 'power2.out' })
     gsap.to('.player__song span', 1.5, { y: 0, autoAlpha: 1, ease: 'power2.out' })
-    gsap.to('.home > .home__welcome >  span span', 1.2, { y: 0, stagger: .1, ease: 'power2.out', opacity: 1 });
-    gsap.to('.home .home__welcome--shadow span span', 1.2, { y: 0, stagger: .1, ease: 'power2.out', opacity: 1 });
+    gsap.to('.home > .home__welcome >  span span', 1.2, { y: 0, stagger: .15, ease: 'power2.out', opacity: 1 });
   }, timeout)
 }
 export const homeLeave = () => {
