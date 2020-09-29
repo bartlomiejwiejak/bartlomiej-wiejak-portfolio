48b5bd76eaa301a6375ec80f44a2ea53d6ebcf98
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-09-29 06:22:48 +0200

    Fixed cursor

--- src/hooks/useResize.js
+++ src/hooks/useResize.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 
+import isMobile from '../functions/isMobile';
+
 const useResize = (callback) => {
   useEffect(() => {
     const html = document.querySelector('html');
@@ -8,11 +10,21 @@ const useResize = (callback) => {
     let fontSize = 10 - (1920 - window.innerWidth) / 300 + 'px';
     html.style.fontSize = fontSize;
     documentEl.style.setProperty('--vh', `${vh}px`);
+    if (isMobile()) {
+      document.querySelector('.cursor').style.display = 'none';
+    } else {
+      document.querySelector('.cursor').style.display = 'block';
+    }
     function handleResize() {
       fontSize = 10 - (1920 - window.innerWidth) / 300 + 'px';
       vh = window.innerHeight * 0.01;
       html.style.fontSize = fontSize;
       documentEl.style.setProperty('--vh', `${vh}px`);
+      if (isMobile()) {
+        document.querySelector('.cursor').style.display = 'none';
+      } else {
+        document.querySelector('.cursor').style.display = 'block';
+      }
       callback();
     }
     window.addEventListener("resize", handleResize);
