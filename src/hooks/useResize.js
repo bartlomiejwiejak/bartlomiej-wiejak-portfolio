474507d420f2271f1f7c290911e1b103aa2d4c15
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-10-04 12:45:49 +0200

    Optimalization

--- src/hooks/useResize.js
+++ src/hooks/useResize.js
@@ -4,18 +4,17 @@ const useResize = (callback) => {
   useEffect(() => {
     const html = document.querySelector('html');
     const documentEl = document.documentElement;
-    let vh = window.innerHeight * 0.01;
-    let fontSize = 10 - (1920 - window.innerWidth) / 300 + 'px';
-    html.style.fontSize = fontSize;
-    documentEl.style.setProperty('--vh', `${vh}px`);
 
     function handleResize() {
-      fontSize = 10 - (1920 - window.innerWidth) / 300 + 'px';
-      vh = window.innerHeight * 0.01;
+      const fontSize = 10 - (1920 - window.innerWidth) / 300 + 'px';
+      const vh = window.innerHeight * 0.01;
       html.style.fontSize = fontSize;
       documentEl.style.setProperty('--vh', `${vh}px`);
       callback();
     }
+
+    handleResize();
+
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, [callback]);
