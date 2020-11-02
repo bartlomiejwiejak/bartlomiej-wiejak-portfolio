a44cd038205097a1be3d311d05de614bd0510011
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-11-02 15:55:31 +0100

    Update links

--- src/components/views/Home/Contact.js
+++ src/components/views/Home/Contact.js
@@ -11,13 +11,13 @@ const Contact = () => {
       <aside className='contact'>
         <ul className='contact__list'>
           <li className='contact__item'>
-            <Button href='https://linkedin.com/in/barłomiej-wiejak-3431941b9' type='white'>LinkedIn</Button>
+            <Button href='https://www.linkedin.com/in/bart%C5%82omiej-wiejak-3431941b9' type='white'>LinkedIn</Button>
           </li>
           <li className='contact__item'>
             <Button href='https://github.com/bartlomiejwiejak' type='white'>Github</Button>
           </li>
           <li className='contact__item'>
-            <Button href='https://www.facebook.com/wiejaczek/' type='white'>Facebook</Button>
+            <Button href='https://www.facebook.com/bartlomiejwiejak' type='white'>Facebook</Button>
           </li>
         </ul>
       </aside>
